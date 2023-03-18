931d8556359e7669aa9d210124d964afa23b1290
pasichniuk <vlad.pasichniuk.02@gmail.com>
2023-03-18 23:35:34 +0200

    Change error message

--- src/main/java/com/crypto/advisor/exception/CryptoNotFoundException.java
+++ src/main/java/com/crypto/advisor/exception/CryptoNotFoundException.java
@@ -3,6 +3,6 @@ package com.crypto.advisor.exception;
 public class CryptoNotFoundException extends IllegalArgumentException {
 
     public CryptoNotFoundException(String symbol) {
-        super(String.format("Couldn't find cryptocurrency with symbol: %s", symbol));
+        super(String.format("Couldn't find cryptocurrency with symbol '%s'", symbol));
     }
 }
